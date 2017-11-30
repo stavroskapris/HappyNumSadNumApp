c5c5c1e0fcba57e37d35229bcb6e72a0e5c08fce
StavrosKapris <33174615+StavrosKapris@users.noreply.github.com>
2017-11-30 23:01:44 +0200

    formatted

--- src/happySad/pack/NumberController.java
+++ src/happySad/pack/NumberController.java
@@ -8,59 +8,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-
 public class NumberController {
 	@RequestMapping("/")
 	public String ShowForm() {
-
 		return "helloworld-form";
-
 	}
 
 	@RequestMapping("proccesApp")
-	public String dotheJob(@RequestParam("number") int x,RedirectAttributes redAtt) {
-		boolean result=false;
+	public String dotheJob(@RequestParam("number") int x, RedirectAttributes redAtt) {
+		boolean result = false;
 		ArrayList<Integer> sums = new ArrayList<Integer>();
-
 		int number = x;
 		if (x == 1)
 			result = true;
 		while (number != 1) {
-
 			int sum = 0;
 			ArrayList<Integer> digits = new ArrayList<Integer>();
-
 			while (number > 0) {
-
 				digits.add(number % 10);
 				number = number / 10;
 			}
-
 			for (int i = 0; i < digits.size(); i++) {
 				sum = sum + digits.get(i) * digits.get(i);
 			}
 			// System.out.println(digits);
-
 			number = sum;
 			if (sums.contains(number)) {
 				result = false;
 				break;
 			}
 			sums.add(number);
-			System.out.println(sums);
+			// System.out.println(sums);
 
 			if (sum == 1) {
 				result = true;
 			}
-
 		}
-		
 		if (result) {
-			
 			redAtt.addAttribute("number", x);
 			redAtt.addFlashAttribute("message1", "is Happy Number");
 			return "redirect:/";
-			
 		} else {
 			redAtt.addAttribute("number", x);
 			redAtt.addFlashAttribute("message2", "is Sad Number");
